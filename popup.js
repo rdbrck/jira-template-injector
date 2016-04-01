458eb936950bd0c8484676d50d4257cfdf1d46da
Conrad Foucher <conrad.foucher@redbrickmedia.com>
2016-04-01 16:24:53 -0700

    Loading and reading from chrome storage. Started on pop up window. Really need a designer!

--- popup.js
+++ popup.js
@@ -1,1 +1,17 @@
-popup.js
+var PopupController = function () {
+  this.button_ = document.getElementById('button');
+  this.timeframe_ = document.getElementById('timeframe');
+  this.addListeners_();
+};
+
+PopupController.prototype = {
+
+    addListeners_: function () {
+        this.button_.addEventListener('click', this.handleClick_.bind(this));
+    },
+
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  window.PC = new PopupController();
+});
