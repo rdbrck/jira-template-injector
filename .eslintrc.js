33da2fcb47e2c2d7dd36991f830e80b2273d9d33
Conrad Foucher <conrad.foucher@redbrickmedia.com>
2016-07-04 09:55:09 -0700

    EXT-126: Starting to lint js files

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "extends": "standard",
+    "installedESLint": true,
+    "plugins": [
+        "standard"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "env": {
+        "browser": true,
+        "jquery": true,
+        "es6": true
+    },
+    "rules": {
+        "indent": ["error", 4],
+        "semi": ["error", "always", { "omitLastInOneLineBlock": true}],
+        "one-var": "off"
+    }
+};
