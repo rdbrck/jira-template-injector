2b74080f12c7ecbf36d71f104ce290d639b82b97
cfoucher <conrad@foucher.ca>
2016-07-04 18:03:24 -0400

    Ext 126 code analysis (#12)
    * EXT-126: Starting to lint js files
    
    * EXT-126: Starting to lint js files
    
    * eslint requires plugin-promise
    
    * EXT-126: Updated most files to conform with eslint
    
    * EXT-126: Last update for linting
    
    * EXT-126: Ignore json files

--- src/jti_content.js
+++ src/jti_content.js
@@ -1,12 +1,14 @@
 /* Copyright 2016 Redbrick Technologies, Inc. */
 /* https://github.com/rdbrck/jira-description-extension/blob/master/LICENSE */
 
-var StorageID = "Jira-Template-Injector";
+/* global chrome */
 
-$(document).on('click', "#description", function () {
+var StorageID = 'Jira-Template-Injector';
+
+$(document).on('click', '#description', function () {
     var text = $(this).val(),
-        cursorStart = $(this).prop("selectionStart"),
-        cursorFinish = $(this).prop("selectionEnd"),
+        cursorStart = $(this).prop('selectionStart'),
+        cursorFinish = $(this).prop('selectionEnd'),
         end = (text.length - 5),
         selectStart = null,
         selectEnd = null,
@@ -14,15 +16,15 @@ $(document).on('click', "#description", function () {
 
     // Only proceed if this is a click. i.e. not a highlight
     if (cursorStart === cursorFinish) {
-        // Look for opening tag "<DT>"
+        // Look for opening tag '<DT>'
         for (i = cursorStart; i >= 4; i--) {
             if (i !== 4) {
-                if (text.slice((i - 5), i) === "</TI>") {
+                if (text.slice((i - 5), i) === '</TI>') {
                     // Found closing tag before opening tag -> We are not withing any valid tags
                     break;
                 }
             }
-            if (text.slice((i - 4), i) === "<TI>") {
+            if (text.slice((i - 4), i) === '<TI>') {
                 // Found opening Tag!
                 selectStart = (i - 4);
                 break;
@@ -30,14 +32,13 @@ $(document).on('click', "#description", function () {
         }
 
         if (selectStart) {
-            // Look for closing tag "</DT>"
+            // Look for closing tag '</DT>'
             for (i = cursorStart; i <= end; i++) {
-
-                if (text.slice(i, (i + 4)) === "<TI>") {
+                if (text.slice(i, (i + 4)) === '<TI>') {
                     // Found another opening bracket before closing bracket. Exit search
                     break;
                 }
-                if (text.slice(i, (i + 5)) === "</TI>") {
+                if (text.slice(i, (i + 5)) === '</TI>') {
                     // Found closing Tag!
                     selectEnd = (i + 5);
                     break;
@@ -52,29 +53,29 @@ $(document).on('click', "#description", function () {
 });
 
 // On submit send analytics to Desk Metrics
-$(document).on('click', "#create-issue-submit", function () {
+$(document).on('click', '#create-issue-submit', function () {
     chrome.runtime.sendMessage({type: 'analytics', name: 'issue_type', body:
-        {
-            "type": $('#issuetype-field').val(),
-            "version": chrome.runtime.getManifest().version,
-            "ip_address": "${dm.meta:request_ip}",
-            "geo": "${dm.meta:request_geo}",
-            "ua": "${dm.ua:user_agent}"
-        }
+    {
+        'type': $('#issuetype-field').val(),
+        'version': chrome.runtime.getManifest().version,
+        'ip_address': '${dm.meta:request_ip}',
+        'geo': '${dm.meta:request_geo}',
+        'ua': '${dm.ua:user_agent}'
+    }
     });
 });
 
-function isDefaultDescription(value, callback) {
+function isDefaultDescription (value, callback) {
     chrome.storage.sync.get(StorageID, function (templates) {
         templates = templates[StorageID].templates;
         var match = false;
 
         // Check if it's empty
-        if (value === "") {
+        if (value === '') {
             match = true;
         }
 
-        //Check if we've already loaded a template
+        // Check if we've already loaded a template
         if (!match) {
             $.each(templates, function (key, template) {
                 if (value === template.text) {
@@ -88,14 +89,13 @@ function isDefaultDescription(value, callback) {
     });
 }
 
-
-function injectDescriptionTemplate(descriptionElement) {
+function injectDescriptionTemplate (descriptionElement) {
     // Each issue type can have its own template
     chrome.storage.sync.get(StorageID, function (templates) {
         templates = templates[StorageID].templates;
 
         // Load default template if set. Individual Templates will over ride it.
-        var templateText = "",
+        var templateText = '',
             issueTypeElement = $('#issuetype-field');
 
         if (templates['DEFAULT TEMPLATE']) {
@@ -111,28 +111,27 @@ function injectDescriptionTemplate(descriptionElement) {
             });
             descriptionElement.value = templateText;
         } else {
-            console.error("*** Error: Element Id 'issuetype-field' not found.");
+            console.error('*** Error: Element Id "issuetype-field" not found.');
         }
     });
 }
 
-
-function descriptionChangeEvent(changeEvent) {
+function descriptionChangeEvent (changeEvent) {
     // the description field has been changed, turn the dirtyDialogMessage back on and remove the listener
-    changeEvent.target.className = changeEvent.target.className.replace(" ajs-dirty-warning-exempt", "");
-    changeEvent.target.removeEventListener("change", descriptionChangeEvent);
+    changeEvent.target.className = changeEvent.target.className.replace(' ajs-dirty-warning-exempt', '');
+    changeEvent.target.removeEventListener('change', descriptionChangeEvent);
 }
 
-function observeDocumentBody(mutation) {
-    if (document.getElementById("create-issue-dialog") !== null) { // only interested in document changes related to Create Issue Dialog box
-        if (mutation.target.id === "description") { // only interested in the description field
+function observeDocumentBody (mutation) {
+    if (document.getElementById('create-issue-dialog') !== null) { // only interested in document changes related to Create Issue Dialog box
+        if (mutation.target.id === 'description') { // only interested in the description field
             var descriptionElement = mutation.target;
             isDefaultDescription(descriptionElement.value, function (result) {
                 if (result) { // only inject if description field has not been modified by the user
                     injectDescriptionTemplate(descriptionElement);
-                    if (descriptionElement.className.indexOf("ajs-dirty-warning-exempt") === -1) { // default template injection should not pop up dirtyDialogMessage
-                        descriptionElement.className += " ajs-dirty-warning-exempt";
-                        descriptionElement.addEventListener("change", descriptionChangeEvent);
+                    if (descriptionElement.className.indexOf('ajs-dirty-warning-exempt') === -1) { // default template injection should not pop up dirtyDialogMessage
+                        descriptionElement.className += ' ajs-dirty-warning-exempt';
+                        descriptionElement.addEventListener('change', descriptionChangeEvent);
                     }
                 }
             });
@@ -144,4 +143,4 @@ function observeDocumentBody(mutation) {
 var observer = new MutationObserver(function (mutations) {
     mutations.forEach(observeDocumentBody);
 });
-observer.observe(document.body, {subtree: true, attributes: true, attributeFilter: ["resolved"]});
+observer.observe(document.body, {subtree: true, attributes: true, attributeFilter: ['resolved']});
