65ad6e7c1d204d5af79a6b80da4f6640bd3fbd7f
automateddeploy <automateddeploy@redbrickmedia.com>
2017-01-30 11:16:01 -0800

    update

--- ci/register_pipelines.sh
+++ ci/register_pipelines.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-~/fly -t rdbrck login --concourse-url https://concourse2.rdbrck.com/
-~/fly -t rdbrck set-pipeline -c pipeline.yml -p jira-template-injector -l credentials.yml
+fly -t rdbrck login --concourse-url https://concourse2.rdbrck.com/
+fly -t rdbrck set-pipeline -c pipeline.yml -p jira-template-injector -l credentials.yml
