159bb5313ff4f53f2888bd960dfa2e6f77921dd2
Alexander Laing <laingawbl@gmail.com>
2021-07-13 15:09:41 -0700

    PUB-5649: re-reformat eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,24 @@
 module.exports = {
-    "extends": "standard",
-    "installedESLint": true,
-    "plugins": [
-        "standard"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "ecmaFeatures": {
-            "jsx": true
-        }
+  extends: 'standard',
+  installedESLint: true,
+  plugins: ['standard'],
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true,
     },
-    "env": {
-        "browser": true,
-        "jquery": true,
-        "es6": true
-    },
-    "rules": {
-        "indent": ["error", 4],
-        "semi": ["error", "always", { "omitLastInOneLineBlock": true}],
-        "one-var": "off"
-    },
-    "globals": {
-        "utils": false
-    }
+  },
+  env: {
+    browser: true,
+    jquery: true,
+    es6: true,
+  },
+  rules: {
+    indent: ['error', 4],
+    semi: ['error', 'always', { omitLastInOneLineBlock: true }],
+    'one-var': 'off',
+  },
+  globals: {
+    utils: false,
+  },
 };
